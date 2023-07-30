b3e03681452d9810416ba6f6ec382aa96e5e22b7
Alex <alex@Alexs-MacBook-Pro.local>
2023-07-29 20:05:29 -0600

    changes

--- src/app/factura-venta/factura-venta-nuevo/factura-venta-nuevo.component.ts
+++ src/app/factura-venta/factura-venta-nuevo/factura-venta-nuevo.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Item } from 'src/app/models/item.interface';
 
 @Component({
   selector: 'app-factura-venta-nuevo',
@@ -7,26 +8,58 @@ import { Component } from '@angular/core';
 })
 export class FacturaVentaNuevoComponent {
   rows: any[] = [];
+  items: Item[] = []
 
   // Define the options for dropdowns (you can populate this from your data source)
-  dropdownOptions: string[] = ['Option 1', 'Option 2', 'Option 3'];
+  
   total: number = 0;
 
   constructor() {
     // Add a new row with default values when the component is created
     this.addRow();
+    this.items.push({
+      itemName: 'Activacion NIT',
+      reference: 'Reference 1',
+      price: 100,
+      tax: 100,
+      discount: 0,
+      charge: 0,
+      description: "desc1"
+    });
+
+    this.items.push({
+      itemName: 'Volumen Documento Electronico',
+      reference: 'Reference 2',
+      price: 200,
+      tax: 200,
+      discount: 0,
+      charge: 0,
+      description: "desc2"
+    });
+
+    this.items.push({
+      itemName: 'TEPRE',
+      reference: 'Reference 3',
+      price: 300,
+      tax: 300,
+      discount: 0,
+      charge: 0,
+      description: "desc3"
+    });
+
   }
 
   addRow(): void {
     // Add a new row with default values
     this.rows.push({
-      column1: this.dropdownOptions[0],
-      column2: '',
-      column3: '',
-      column4: this.dropdownOptions[0],
-      column5: '',
-      column6: '',
-      column7: 0, // Initialize column7 to 0 (as a number)
+      column1: '',
+      column2: '', //reference
+      column3: '0', //price
+      column4: 0, //tax
+      column5: '',//desc
+      column6: '0',//dicscount
+      column7: 1, // qty
+      column8: '0',// total
     });
 
     // Recalculate the total
@@ -44,14 +77,48 @@ export class FacturaVentaNuevoComponent {
   // Calculate the total value from column7 of all rows
   calculateTotal(): void {
     this.total = this.rows.reduce((sum, row) => {
-      const column7Value = parseFloat(row.column7) || 0;
-      const column6Value = parseFloat(row.column6) || 0;
-      return sum + column7Value * column6Value;
+      const column8Value = parseFloat(row.column8) || 0; // Use column8 for calculation
+      return sum + column8Value;
     }, 0);
   }
 
   onColumnChange(): void {
-    // Called whenever the values of column6 or column7 change
+    // Called whenever the values of any relevant column change
+    this.rows.forEach((row, index) => {
+      this.calculateRowTotal(index);
+    });
+  }
+
+  getItemReference(itemName: string): string {
+    // Find the item with the given itemName and return its reference
+    const item = this.items.find((item) => item.itemName === itemName);
+    return item ? item.reference : '';
+  }
+  getItemPrice(itemName: string): string {
+    // Find the item with the given itemName and return its price as a string
+    const item = this.items.find((item) => item.itemName === itemName);
+    return item ? item.price.toString() : '';
+  }
+
+  getItemDescription(itemName: string): string {
+    // Find the item with the given itemName and return its description
+    const item = this.items.find((item) => item.itemName === itemName);
+    return item ? item.description : '';
+  }
+  getItemTax(itemName: string): string {
+    // Find the item with the given itemName and return its description
+    const item = this.items.find((item) => item.itemName === itemName);
+    return item ? item.tax.toString() : '';
+  }
+  calculateRowTotal(rowIndex: number): void {
+    const row = this.rows[rowIndex];
+    const price = parseFloat(row.column3) || 0;
+    const quantity = parseFloat(row.column7) || 0;
+    const discount = parseFloat(row.column6) || 0;
+    const total = (price * quantity - discount).toFixed(2);
+    // Set the calculated total in the corresponding row
+    this.rows[rowIndex].column8 = total;
+    // Recalculate the total for all rows
     this.calculateTotal();
   }
 }
---
+++ src/app/models/item.interface.ts
@@ -0,0 +1,10 @@
+export interface Item {
+    itemName: string;
+    reference: string;
+    description: string;
+    price: number;
+    tax: number;
+    discount: number;
+    charge: number;
+  }
+  
